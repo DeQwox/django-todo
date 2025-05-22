b482c469a799fc15886c28e21d2fc00fcddfacbe
Artem Hryshchenko <artemgrisenko039@gmail.com>
2025-05-22 21:56:53 +0300

    Add task_list view

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    return render(request, 'todo/task_list.html', {'tasks': tasks, 'form': form})
